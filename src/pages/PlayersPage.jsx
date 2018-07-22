d953da32a614149f9eead41ba927b7250993249d
boggsey <eric@mlemedia.com>
2018-07-22 11:33:30 -0400

    Adding the roster page

---
+++ src/pages/PlayersPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlayersPage = () => (
+  <div>
+    This is the players page!
+  </div>
+);
+
+export default PlayersPage;
---
+++ src/pages/RosterPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RosterPage = () => (
+  <div>
+    This is the roster page!
+  </div>
+);
+
+export default RosterPage;
