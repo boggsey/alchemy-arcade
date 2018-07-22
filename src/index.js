e33f1bfdf8b0a1d9ad2482bfde1f8a8e9675cbd4
boggsey <eric@mlemedia.com>
2018-07-22 11:47:05 -0400

    Adding BrowserRouter from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Importing the fetch polyfill allows cypress to intercept fetch api requests.
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'whatwg-fetch';
-// Change me if you prefer sass,scss, less. (Note you may need to update the build config)
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
 
 // Hot Module Replacement
 if (module.hot) {
